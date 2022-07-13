90e1331056d9277c56375421143609b6a6f17562
Jon <jon@jonrose.dev>
2022-07-13 00:29:05 -0400

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './App.css';
+
+const rowOffset = ({ rowIdx, sqrt }) => rowIdx * sqrt;
+const isEmptyRow = ({ rowIdx, itemCount, sqrt }) => rowOffset({ rowIdx, sqrt }) >= itemCount;
+const itemNumber = ({ rowIdx, sqrt, colIdx }) => rowOffset({ rowIdx, sqrt }) + colIdx + 1;
+const isEmptyItem = ({ colIdx, itemCount, sqrt, rowIdx }) => itemNumber({ rowIdx, sqrt, colIdx }) > itemCount;
+const fillArray = (count) => [...Array(count).keys()];
 
 export const App = () => {
   const [itemCount, setItemCount] = useState(3);
   const sqrt = itemCount > 2 ? Math.ceil(Math.sqrt(itemCount)) : 1;
   const columnCount = sqrt < 2 ? itemCount : sqrt;
+  const onChange = (e) => setItemCount(Number(e.target.value));
+
   return (
-    <div className='App'>
+    <div>
       <label htmlFor="number">Number of items</label>
-      <input value={itemCount} onChange={(e) => setItemCount(e.target.value)} id="number" type="number" />
+      <input value={itemCount} onChange={onChange} id="number" type="number" />
       <div className="container">
-        {Array.from({ length: sqrt }, (_, rowIdx) => {
-          const isEmpty = (rowIdx * sqrt) >= itemCount;
-          return (
-            <div className={`row${isEmpty ? ' empty' : ''}`} key={`row${rowIdx}`}>
-            {Array.from({ length: columnCount }, (_, colIdx) => {
-              const itemNumber = (rowIdx * sqrt) + colIdx + 1;
-              if (itemNumber > itemCount) {
-                return <div key={`col${colIdx}`} className="item placeholder" />
+        {fillArray(sqrt).map((rowIdx) => (
+            <div className={classNames('row', { empty: isEmptyRow({ rowIdx, itemCount, sqrt }) })} key={`row${rowIdx}`}>
+              {fillArray(columnCount).map((colIdx) => 
+                isEmptyItem({ colIdx, itemCount, sqrt, rowIdx }) ? (
+                  <div key={`col${colIdx}`} className="placeholder" />
+                ) : (
+                  <div key={`col${colIdx}`} className="item">
+                    item {itemNumber({ rowIdx, sqrt, colIdx })}
+                  </div>
+                ))
               }
-              return (
-                <div key={`col${colIdx}`} className="item">item {(rowIdx * sqrt) + colIdx + 1}</div>
-              )})
-            }
             </div>
-          )})}
+          ))}
       </div>
     </div>
   );
