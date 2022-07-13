60a64792101295c80b46f148b601b36290e47ed0
Jon <jon@jonrose.dev>
2022-07-12 23:25:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+export const App = () => {
+  const [itemCount, setItemCount] = useState(3);
+  const sqrt = itemCount > 2 ? Math.ceil(Math.sqrt(itemCount)) : 1;
+  const columnCount = sqrt < 2 ? itemCount : sqrt;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <label htmlFor="number">Number of items</label>
+      <input value={itemCount} onChange={(e) => setItemCount(e.target.value)} id="number" type="number" />
+      <div className="container">
+        {Array.from({ length: sqrt }, (_, rowIdx) => {
+          const isEmpty = (rowIdx * sqrt) >= itemCount;
+          return (
+            <div className={`row${isEmpty ? ' empty' : ''}`} key={`row${rowIdx}`}>
+            {Array.from({ length: columnCount }, (_, colIdx) => {
+              const itemNumber = (rowIdx * sqrt) + colIdx + 1;
+              if (itemNumber > itemCount) {
+                return <div key={`col${colIdx}`} className="item placeholder" />
+              }
+              return (
+                <div key={`col${colIdx}`} className="item">item {(rowIdx * sqrt) + colIdx + 1}</div>
+              )})
+            }
+            </div>
+          )})}
+      </div>
     </div>
   );
 }
-
-export default App;
